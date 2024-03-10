fc999b5816cf0107335830a40d3d96d089c7bb69
Neerukattu <sxn2261@mavs.uta.edu>
2024-03-10 12:06:48 -0500

    frontend updated

--- omnimart-storefront/src/modules/home/components/hero/index.tsx
+++ omnimart-storefront/src/modules/home/components/hero/index.tsx
@@ -10,17 +10,17 @@ const Hero = () => {
             level="h1"
             className="text-3xl leading-10 text-ui-fg-base font-normal"
           >
-            Ecommerce Starter Template
+            Omnimart
           </Heading>
           <Heading
             level="h2"
             className="text-3xl leading-10 text-ui-fg-subtle font-normal"
           >
-            Powered by Medusa and Next.js
+            Made by Harshitha, Sathwik , Harshini, Surya Teja
           </Heading>
         </span>
         <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
+          href="https://github.com/SuryaTeja-N/Omnimart-Store"
           target="_blank"
         >
           <Button variant="secondary">
--- omnimart-storefront/src/modules/layout/components/side-menu/index.tsx
+++ omnimart-storefront/src/modules/layout/components/side-menu/index.tsx
@@ -84,8 +84,7 @@ const SideMenu = ({ regions }: { regions: Region[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
-                        reserved.
+                        © {new Date().getFullYear()} Omnimart
                       </Text>
                     </div>
                   </div>
